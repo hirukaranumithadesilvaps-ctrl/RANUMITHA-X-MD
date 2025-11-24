1611507564dacbfeee7191d8ee7b59a29203d4cf
hirukaranumithadesilvaps-ctrl <hirukaranumithadesilvaps@gmail.com>
2025-11-25 01:38:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ranu&MEoXWB5S#qj-3b_pCSjid7MNxLOFu2OKMSlmhflAKeVT87TxLcSw",
+SESSION_ID: process.env.SESSION_ID || "ranu&0NAEDRIR#sn7u_vtMMp50qHVl5CWrFUuwn_PqXxCjIBrtVHkXt-0",
 // add your Session Id (මුලට "ranu&")
 AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
 // make it true for auto recoding 
